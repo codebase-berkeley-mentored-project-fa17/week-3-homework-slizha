e876def67155e3507ca513fa2970f3fc4398a068
Sarah Hammond <s.hammond@berkeley.edu>
2017-10-15 12:47:28 -0700

    initial commit

--- bloggo/posts/views.py
+++ bloggo/posts/views.py
@@ -1,26 +1,29 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_4pk = 04
 from django.urls import reverse
 
 from .models import Post
 
 # Create your views here.
 
+
 def index(request):
     posts = Post.objects.order_by("-pub_date")
     return render(request, "posts/index.html", {"posts": posts})
 
+
 def about(request):
     #################################
     # Question 1
-    # REPLACE THE LINE WITH YOUR CODE
-    return HttpResponse("No about page :(")
+    posts = Post.objects.order_by("-pub_date")
+    return render(request, "posts/about.html")
+
 
 def post_details(request, pk):
     #################################
     # Question 2
     # You should create a new file in the templates directory.
-    # REPLACE THE LINE WITH YOUR CODE
-    return HttpResponse("No post details page :(")
+    post = Post.objects.get(pk=pk)
+    return render(request, "posts/post_details.html", {"post": post})
